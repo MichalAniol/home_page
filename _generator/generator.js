f101125230d8ccd1c50f4e393b723a87a382be5b
MichalAniol <michal.janusz.aniol@gmail.com>
2022-08-04 11:49:43 +0200

    generator + svg fixes

---
+++ _generator/generator.js
@@ -0,0 +1,115 @@
+const oof = require('./operationsOnFiles');
+
+const start = () => {
+    let combinedHtml = '';
+
+    const svgMinify = code => {
+        let splitted = code.toString().split('\n');
+        let res = '';
+
+        for (let s of splitted) {
+            let lineStart = 0;
+            for (let char of s) {
+                if (char === ' ') {
+                    lineStart++;
+                } else {
+                    break;
+                }
+            }
+
+            let line = s.replace('\r', '').substring(lineStart, 10000000);
+
+            res += line
+        }
+
+        return res;
+    }
+
+    const minify = code => {
+        let splitted = code.toString().split('\n');
+        let res = '';
+
+        for (let s of splitted) {
+            let lineStart = 0;
+            for (let char of s) {
+                if (char === ' ') {
+                    lineStart++;
+                } else {
+                    break;
+                }
+            }
+
+            let line = s.replace('\r', '').substring(lineStart, 10000000);
+
+            let svgOccurs = line.indexOf('.svg');
+            if (svgOccurs > -1) {
+                let img = line.indexOf('<img');
+                let restOfLine = line.substring(img);
+                let endImg = restOfLine.indexOf('/>');
+
+                let leftLine = line.substring(0, img);
+                let rightLine = line.substring(img + endImg + 2, 10000000);
+
+                let srcSplitted = restOfLine.split('"');
+                let src = srcSplitted[1].substring(2, 10000000);
+
+                let svg = svgMinify(oof.loadSvg(src));
+
+                line = leftLine + svg + rightLine;
+            }
+
+            res += line
+        }
+
+        return res;
+    }
+
+    const htmlList = [
+        'head',
+
+        'base/main',
+        'base/video',
+        'base/shopping',
+        'base/news',
+        'base/life',
+        'base/time',
+        'base/inspirations',
+        'base/helpers',
+        'base/knowledge',
+        'base/portals',
+        'base/maps',
+        'base/programmes',
+        'base/searchEngines',
+        'base/sounds',
+        'base/retro',
+        'base/games',
+        'base/business',
+
+        'programingSection',
+
+        'programming/work',
+        'programming/css',
+        'programming/html',
+        'programming/fonts',
+        'programming/javaScript',
+        'programming/colors',
+        'programming/otherStuff',
+        'programming/graphics',
+        'programming/canvas',
+        'programming/frameworks',
+        'programming/mobileApps',
+        'programming/specials',
+        'programming/materials',
+        'programming/codeNotes',
+        'programming/waitingRoom',
+
+        'end',
+    ]
+
+    htmlList.forEach(e => combinedHtml += minify(oof.load(e + '.html')));
+
+    console.log('---------------------------------');
+    oof.save('index', combinedHtml);
+}
+
+module.exports = { start };
---
+++ _generator/operationsOnFiles.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+const path = require('path');
+
+const path_in = path.join(__dirname, 'splittedHtml');
+
+let splitted = __dirname.split('\\');
+let path_out = '';
+splitted.forEach((e, i) => i < splitted.length - 1 ? path_out += e + '/' : null);
+
+
+
+const load = name => {
+    const filePath = path_in + '/' + name;
+    let data = null;
+
+    try {
+        if (fs.existsSync(filePath)) {
+            data = fs.readFileSync(filePath);
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
+    console.log(' loaded: ' + name);
+
+    return data;
+}
+
+const loadSvg = name => {
+    const filePath = path_out + '/' + name;
+    let data = null;
+
+    try {
+        if (fs.existsSync(filePath)) {
+            data = fs.readFileSync(filePath);
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
+    console.log(' loaded svg: ' + name);
+
+    return data;
+}
+
+const loadByPat = path => {
+    let data = null;
+
+    try {
+        if (fs.existsSync(path)) {
+            data = fs.readFileSync(path);
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
+    console.log(' loaded: ' + path);
+
+    return data;
+}
+
+const readAllFiles = folder => {
+    const getDir = path => {
+        var files = fs.readdirSync(path);
+        let res = [];
+
+        for (let file of files) {
+            const newPath = path + '/' + file;
+            const obj = {};
+            if (fs.lstatSync(newPath).isDirectory()) {
+                const dir = getDir(newPath);
+                obj[file] = dir;
+                res.push(obj);
+            } else {
+                obj[file] = loadByPat(newPath).toString();
+                res.push(obj);
+            }
+        }
+
+        return res;
+    }
+
+    const filePath = path_in + '/' + folder;
+
+    return getDir(filePath);
+}
+
+const save = (name, data) => {
+    filePath = path_out + '/' + name + '.html';
+    fs.writeFileSync(filePath, data);
+
+    console.log(' saved file: ' + filePath);
+}
+
+module.exports = { load, loadSvg, readAllFiles, save }
